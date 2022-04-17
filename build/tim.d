9fa7e834f46351dbbee510189b421e0494e16ef8
perinze <perinzefaper@foxmail.com>
2022-04-17 15:42:03 +0800

    high fidelity 114514

--- build/tim.d
+++ build/tim.d
@@ -29,7 +29,8 @@ build/tim.o: Core/Src/tim.c Core/Inc/tim.h Core/Inc/main.h \
  Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim.h \
  Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim_ex.h \
  Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart.h \
- Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart_ex.h Core/Inc/main.h
+ Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart_ex.h Core/Inc/main.h \
+ Core/Inc/usart.h
 Core/Inc/tim.h:
 Core/Inc/main.h:
 Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h:
@@ -66,3 +67,4 @@ Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim_ex.h:
 Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart.h:
 Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart_ex.h:
 Core/Inc/main.h:
+Core/Inc/usart.h:
